db51556c3e3794db6ae336b05af10c44d883f5c0
Naruki Kochi <e235709@ie.u-ryukyu.ac.jp>
2024-01-22 13:14:14 +0900

    playGame

--- app/src/main/java/jp/ac/uryukyu/ie/e235709/HitAndBlow.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235709/HitAndBlow.java
@@ -1,5 +1,6 @@
 package jp.ac.uryukyu.ie.e235709;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -18,8 +19,9 @@ public class HitAndBlow {
             secretNumber[i] = random.nextInt(10);
         }
     }
-    //メソッド2: ヒットの数を数える
-    public int countHits(int [] guess){
+
+    // メソッド2: ヒットの数を数える
+    public int countHits(int[] guess) {
         int hits = 0;
         for (int i = 0; i < 3; i++) {
             if (guess[i] == secretNumber[i]) {
@@ -28,10 +30,11 @@ public class HitAndBlow {
         }
         return hits;
     }
-    //メソッド3: blowの数を数える
-    public int countBlows(int [] guess){
+
+    // メソッド3: blowの数を数える
+    public int countBlows(int[] guess) {
         int blows = 0;
-        for (int i = 0; i <3; i++){
+        for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (i != j && guess[i] == secretNumber[j]) {
                     blows++;
@@ -39,19 +42,49 @@ public class HitAndBlow {
             }
         }
         return blows;
-        }
-    //メソッド4:3桁の数字を入力
+    }
+
+    // メソッド4:3桁の数字を入力
     public int[] takeUserGuess() {
-        Scanner scanner = new Scanner(System.in);//入力処理を行う
+        Scanner scanner = new Scanner(System.in);// 入力処理を行う
         int[] guess = new int[3];
 
         System.out.print("3桁の数字を入力してください: ");
-        String input = scanner.next();//ユーザーの入力を文字列として取得
+        String input = scanner.next();// ユーザーの入力を文字列として取得
 
         for (int i = 0; i < 3; i++) {
-            guess[i] = Character.getNumericValue(input.charAt(i));//i番目の文字を取得し、数字に変換
+            guess[i] = Character.getNumericValue(input.charAt(i));// i番目の文字を取得し、数字に変換
         }
 
         return guess;
+    }
+
+    // メソッド5:ゲームの制御
+    public void playGame() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hit and Blowゲームを始めます！");
+
+        int[] guess;
+        int attemptCount = 0;
+
+        do {
+            // ゲームのループ処理
+            attemptCount++;// 試行回数のカウント
+            guess = takeUserGuess();
+
+            if (Arrays.equals(guess, secretNumber)) {
+                // 正解した場合の処理
+                System.out.println("おめでとうございます！正解です！");
+                System.out.println("試行回数: " + attemptCount);// 試行回数を表示しループを終了
+                break;
+            } else {
+                // 不正解した場合の処理
+                int hits = countHits(guess);// ヒットの数をカウント
+                int blows = countBlows(guess);// blowの数をカウント
+                System.out.println("Hits: " + hits + ", Blows: " + blows);// hitとblowの数を表示
+            }
+
+        } while (true);
     }
 }
