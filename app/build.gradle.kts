a38ea84b2a52af402a7d48ee5661a4103ec4d20b
Naruki Kochi <e235709@ie.u-ryukyu.ac.jp>
2024-01-23 16:35:48 +0900

    fix build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,6 +9,9 @@ plugins {
     // Apply the application plugin to add support for building a CLI application in Java.
     application
 }
+java {                                      
+    sourceCompatibility = JavaVersion.VERSION_17
+}
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -34,10 +37,18 @@ java {
 
 application {
     // Define the main class for the application.
-    mainClass.set("jp.ac.uryukyu.ie.e235709.App")
+    mainClass.set("Main")
 }
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+tasks.named("run", JavaExec::class.java) {
+    standardInput = System.`in`
+}
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "Main"
+    }
 }
